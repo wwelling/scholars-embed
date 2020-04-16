09c27a938892daff93102002591942616c07cf72
William Welling <wwelling@library.tamu.edu>
2020-04-16 10:00:20 -0500

    add webpage formalize mapping

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     formalize: {
         otherUniversity: 'ExternalOrganization',
         GreyLiterature: 'InstitutionalRepositoryDocument',
+        Webpage: 'InternetPublication',
         ERO_0000071: 'Software'
     }
 };
